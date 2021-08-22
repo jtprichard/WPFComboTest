e51c9d6867f751f9d44b144ff2d8fc12b0e1220c
Jason Prichard Desktop <jprichard@performancebim.com>
2021-08-22 12:23:41 -0400

    Add project files.

---
+++ WPFComboTest/ComboItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFComboTest
+{
+    public class ComboItem
+    {
+        public int Item { get; private set; }
+        public string Description { get; private set; }
+
+        public ComboItem() { }
+
+        public static ObservableCollection<ComboItem> Populate()
+        {
+            ObservableCollection<ComboItem> items = new ObservableCollection<ComboItem>
+            {
+                new ComboItem {Item = 0, Description = "Item 1"},
+                new ComboItem {Item = 1, Description = "Item 2"},
+                new ComboItem {Item = 2, Description = "Invalid Item"}
+
+            };
+            return items;
+        }
+    }
+}
---
+++ WPFComboTest/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+
+namespace WPFComboTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static ViewModel _instance;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _instance = new ViewModel();
+            DataContext = _instance;
+        }
+
+        private void ShowComboItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("ComboBox in ViewModel: " + _instance.SelectedComboItem.Description, "Combo Test");
+        }
+
+        private void UpdateVMSelectedItem_Click(object sender, RoutedEventArgs e)
+        {
+            _instance.SelectedComboItem = _instance.ComboItems[0];
+        }
+    }
+}
